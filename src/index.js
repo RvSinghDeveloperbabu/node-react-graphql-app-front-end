78c4a3a74e6532518ac275e843f4be671ff4578f
ravi kumar <ravikumar.developerbabu@gmail.com>
2020-07-12 12:13:42 +0530

    creating CRUD components

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import { ApolloProvider } from 'react-apollo';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Show from './components/Show';
 
 const client = new ApolloClient({ uri: 'http://localhost:3000/graphql'});
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
         <Router>
             <div>
                 <Route exact path='/' component={App} />
@@ -22,8 +22,9 @@ ReactDOM.render(
                 <Route path='/show/:id' component={Show} />
             </div>
         </Router>
+    </ApolloProvider>,
+    document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
