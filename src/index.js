2677657e91d77e60ed418d7ed9b6424271d61d54
ravi kumar <ravikumar.developerbabu@gmail.com>
2020-07-12 12:11:43 +0530

    connet the backend server to front end app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import * as serviceWorker from './serviceWorker';
 
+const client = new ApolloClient({ uri: 'http://localhost:3000/graphql'});
 ReactDOM.render(
   <React.StrictMode>
     <App />
